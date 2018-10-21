b82969f51f16e5268c55895fa1707caf5a16483b
William <wlam98@hotmail.com>
2018-10-21 13:23:32 -0400

    Stages of Rocket Launch activity page.

---
+++ app/src/main/java/spaceappsottawa/launchnow/StagesOfRocketLaunch.java
@@ -0,0 +1,13 @@
+package spaceappsottawa.launchnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StagesOfRocketLaunch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stages_of_rocket_launch);
+    }
+}
