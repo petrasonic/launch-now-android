02dcaad99e2dcbf7916a7ec0ad002ea68f77d88c
William <wlam98@hotmail.com>
2018-10-21 12:59:42 -0400

    Rocket jargon activity page.

---
+++ app/src/main/java/spaceappsottawa/launchnow/RocketJargon.java
@@ -0,0 +1,13 @@
+package spaceappsottawa.launchnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RocketJargon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rocket_jargon);
+    }
+}
